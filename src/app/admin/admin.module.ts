e2e707a8b3c4d8e7b3db1cc56ea2e3e7edfd59eb
José Prieto <jose.c.prieto.c2015@gmail.com>
2023-04-09 17:43:17 -0400

    Feat (config): - Añadidas mock settings para mostrarse

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,12 +6,14 @@ import { AdminComponent } from './admin.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { SettingsModule } from '../repositories/settings/settings.module';
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
+    SettingsModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
