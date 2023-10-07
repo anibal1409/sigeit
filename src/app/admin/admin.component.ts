07cbfdae34de4a478a806165cee394613f10b639
Anibal Fariñas <anibal.antonio1409@gmail.com>
2023-10-07 07:17:00 -0400

    refactor: ajustar redireccion de rutas

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+} from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
 import { SettingsService } from '../repositories/settings/settings.service';
 
 interface optionItem {
@@ -13,7 +24,7 @@ interface optionItem {
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
   title = 'SIGEIT';
   user = {
     img: '',
@@ -78,28 +89,96 @@ export class AdminComponent implements OnInit {
     },
   ];
 
+  pages = [
+    {
+      path: '/dashboard/subjects',
+      title: 'Asignaturas',
+    },
+    {
+      path: '/dashboard/classrooms',
+      title: 'Asignaturas',
+    },
+    {
+      path: '/dashboard/careers',
+      title: 'Carreras',
+    },
+    {
+      path: '/dashboard/departments',
+      title: 'Departamentos',
+    },
+    {
+      path: '/dashboard/schools',
+      title: 'Escuelas',
+    },
+    {
+      path: '/dashboard/scheludes',
+      title: 'Horarios',
+    },
+    {
+      path: '/dashboard/scheludes/planned',
+      title: 'Horarios planificados',
+    },
+    {
+      path: '/dashboard/scheludes/academic-charge',
+      title: 'Horarios/Carga Académica Profesor',
+    },
+    {
+      path: '/dashboard/periods',
+      title: 'Periodos Académicos',
+    },
+    {
+      path: '/dashboard/teachers',
+      title: 'Profesores',
+    },
+    {
+      path: '/dashboard/sections',
+      title: 'Secciones',
+    },
+    {
+      path: '/dashboard/users',
+      title: 'Usuarios',
+    },
+  ];
+
+  sub$ = new Subscription();
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private settingsService: SettingsService
-  ) {}
+  ) { }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
+  }
 
   ngOnInit(): void {
-    const path = this.activatedRoute.children[0].snapshot.routeConfig?.path;
-    if (path) {
-      const item = this.optionList.find((option) => option.value === path);
-      this.menuOption(item);
-    }
+    this.updateTitle(this.router.url);
+    this.sub$.add(
+      this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.updateTitle(event.url);
+        }
+      })
+    );
   }
 
   menuOption(option?: optionItem): void {
     if (option && option.name !== 'Configuraciones') {
-      this.title = option.name;
       this.router.navigate([`/dashboard/${option.value}`]);
     } else if (!option) {
       this.title = 'SIGEIT';
     } else {
       this.settingsService.open();
+    }
+  }
+
+  private updateTitle(url: string): void {
+    const item = this.pages.find((page) => page.path === url);
+    if (item) {
+      this.title = item.title;
+    } else {
+      this.title = 'SIGEIT';
     }
   }
 }
